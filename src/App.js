29070c76080ff68209d22390a75f0afa245a4c1f
yassinaitouailal <yassinaitouailal@gmail.com>
2024-10-23 13:10:50 +0100

    23/10/2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Welcome from './components/welcome';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Welcome to Rendez Vous Web Set 
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Router>
+        <Routes>
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/Register" element={<Register  />} />
+          <Route path="/" element={<Welcome  />} />
+
+        </Routes>
+    </Router>
   );
 }
 
